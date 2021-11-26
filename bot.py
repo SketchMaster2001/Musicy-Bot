cc3785eb740ba72efc3f66ef137594c5d7ca335b
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2021-11-26 08:50:13 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from discord_components import DiscordComponents
 
-bot = commands.Bot(command_prefix="/")
+bot = commands.Bot(command_prefix="/", help_command=None)
 
 bot.load_extension("src.music")
 DiscordComponents(bot)
